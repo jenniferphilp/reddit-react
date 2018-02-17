ea47b487a08ca861d80ebc04af58bcece0e08cfd
jenniferphilp <jennifer.philp@gmail.com>
2018-02-17 11:01:22 -0500

    refactor - create Story component

--- coding-test/src/Stories.js
+++ coding-test/src/Stories.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import _ from 'lodash'
-//import { Col } from 'react-bootstrap';
+import Story from './Story'
+
 
 const Stories = (props) => {
 return(
     props.redditItems.map((item) => {
+    
     let textShortened = _.truncate(item.selftext, { 'length': 500 } )
     let formattedDate = _.truncate(new Date(item.created*1000).toString(), {'length': 25, 'omission': ' ' });
-    //let formattedDate = date.toString();
-    return(
-        <div className="story" key={item.id}>
-            <h2>{item.title} </h2>
-            <h3 className="dateAdded">Date Added: {formattedDate}</h3>
-            <p>By: {item.author}  <div className="upvotes text-center">{item.ups}</div></p>
-            <p>{textShortened}</p>
-            <a href={item.url} target="blank">Click for full story</a>
-        </div>)
+
+        return(
+            <Story
+                item = {item}
+                formattedDate = {formattedDate}
+                textShortened = {textShortened}
+            />
+            )
         })
     )
 }
