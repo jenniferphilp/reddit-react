9c45aa2a691439a81cf8fccb59abf927f72fdf23
jenniferphilp <jennifer.philp@gmail.com>
2018-02-18 18:34:57 -0500

    created first test; re-installed node modules

--- coding-test/src/App.test.js
+++ coding-test/src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import sortReddit  from './helpers';
+// import { getData } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+describe("Handle Sort Functions", () => {
+    it("should sort data by upvotes based on boolean passed in - 'sorted' ", () => {
+        expect(sortReddit(true, [2, 1, 6, 4, 11])).toEqual([
+            1, 2, 4, 6, 11
+        ])
+  })
 });
+
+
+
